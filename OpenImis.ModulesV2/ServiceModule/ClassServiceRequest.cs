efdd78f4e054ae186593bb53fdb94069953225f4
20GHhub19 <58400969+20GHhub19@users.noreply.github.com>
2022-08-08 13:39:53 +0100

    updated files on rest_api_c-sharp module on develop-cs branch on 08_08_22

---
+++ OpenImis.ModulesV2/ServiceModule/ClassServiceRequest.cs
@@ -0,0 +1,194 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace OpenImis.ModulesV2.ServiceModule
+{
+   public class ClassServiceRequest
+    {
+
+        public ClassServiceRequest()
+        {
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source = localhost;" + "Initial Catalog=openimisproductNewdb;" + "User ID=openimis;" + "Password=openimis22;" +
+            "integrated security = SSPI;" + "MultipleActiveResultSets =True;");
+        SqlDataReader dr = null;
+
+        SqlConnection conn_sub = new SqlConnection("Data Source = localhost;" + "Initial Catalog=openimisproductNewdb;" + "User ID=openimis;" + "Password=openimis22;" +
+            "integrated security = SSPI;" + "MultipleActiveResultSets=True;");
+        SqlDataReader dr_sub = null;
+
+        SqlConnection conn_sub2 = new SqlConnection("Data Source = localhost;" + "Initial Catalog=openimisproductNewdb;" + "User ID=openimis;" + "Password=openimis22;" +
+            "integrated security = SSPI;" + "MultipleActiveResultSets=True;");
+        SqlDataReader dr_sub2 = null;
+
+
+
+        public IEnumerable<Dictionary<string, object>> SerializeDr()
+        {
+
+            SqlCommand cmd = new SqlCommand(); //create an instance of Sql command object
+
+            
+            StringBuilder s = new StringBuilder();
+            var results = new List<Dictionary<string, object>>();
+            var results_list_all = new List<Dictionary<string,  object>>();
+            try
+            {
+
+                cmd.CommandTimeout = 60; //specify the time (second)
+                cmd.Connection = conn; // copy connection string
+                cmd.CommandType = CommandType.Text;
+
+                
+                
+
+                //open the connection
+                conn.Open();
+                if (conn.State == ConnectionState.Open)// check the state of connection
+                {
+                    Console.WriteLine("Connection was succesfull \n");
+                    cmd.CommandText = "SELECT * From [openimisproductNewdb].[dbo].[tblServiceProductItems]";
+
+                    //get the query result
+                    dr = cmd.ExecuteReader(CommandBehavior.SingleResult);
+
+                    var cols = new List<string>();
+                    for (var i = 0; i < dr.FieldCount; i++)
+                        cols.Add(dr.GetName(i));
+
+
+                    while (dr.Read())
+                    {
+                        var getId = Convert.ToString(dr["ServiceID"]);
+                        results_list_all.Add(SerializeRow_SubRow_Dr(cols, dr, getId));
+                    }
+                    dr.Close();
+
+                }
+            }
+                catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+                finally
+            {
+                conn.Close(); //close the connection
+            }
+
+            return results_list_all;
+        }
+
+
+        private Dictionary<string,  object> SerializeRow_SubRow_Dr(IEnumerable<string> cols,
+                                                       SqlDataReader dr, string id)
+        {
+            var results = new Dictionary<string, object>();
+            var result_sub_req = new Dictionary<string, object>();
+            SqlCommand cmd_sub = new SqlCommand();
+            SqlCommand cmd_sub2 = new SqlCommand();
+
+
+                    foreach (var col in cols)
+                    {
+                        if (col == "ServiceIDFkSCP")
+                        {
+                            try
+                            {
+                                cmd_sub.CommandTimeout = 60; //specify the time (second)
+                                cmd_sub.Connection = conn_sub; // copy connection string
+                                cmd_sub.CommandType = CommandType.Text;
+
+                                conn_sub.Open();
+                                if (conn_sub.State == ConnectionState.Open) // check the state of connection
+                                {
+                                    cmd_sub.CommandText = "SELECT * From [openimisproductNewdb].[dbo].[tblServiceContainedPackage] where tblServiceContainedPackage.ServiceId =" + id;
+                                    //get the query result
+                                    dr_sub = cmd_sub.ExecuteReader(CommandBehavior.SingleRow);
+                                    var cols_sub = new List<string>();
+                                    for (var i = 0; i < dr_sub.FieldCount; i++)
+                                        cols_sub.Add(dr_sub.GetName(i));
+                                    while (dr_sub.Read())
+                                        results.Add(col, SerializeRowDr(cols_sub, dr_sub));
+                                    dr_sub.Close();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Write(ex.Message);
+                            }
+                            finally
+                            {
+                                conn_sub.Close(); //close the connection
+
+                            }
+
+                        }
+                            
+                        else if(col == "ItemID")
+                        {
+                            try
+                            {
+                                cmd_sub2.CommandTimeout = 60; //specify the time (second)
+                                cmd_sub2.Connection = conn_sub2; // copy connection string
+                                cmd_sub2.CommandType = CommandType.Text;
+
+                                conn_sub2.Open();
+                                if (conn_sub2.State == ConnectionState.Open)
+                                {
+                                    cmd_sub2.CommandText = "SELECT * From [openimisproductNewdb].[dbo].[tblProductContainedPackage] where tblProductContainedPackage.ServiceId =" + id;
+                                    //get the query result
+                                    dr_sub2 = cmd_sub2.ExecuteReader(CommandBehavior.SingleRow);
+                                    var cols_sub2 = new List<string>();
+                                    for (var i = 0; i < dr_sub2.FieldCount; i++)
+                                        cols_sub2.Add(dr_sub2.GetName(i));
+                                    while (dr_sub2.Read())
+                                        results.Add(col, SerializeRowDr(cols_sub2, dr_sub2));
+                                    dr_sub2.Close();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Write(ex.Message);
+                            }
+                            finally
+                            {
+                                conn_sub2.Close(); //close the connection
+
+                            }
+                        }
+                       
+                        else
+                            results.Add(col, dr[col]);      
+                    }
+            
+            return results;
+        }
+
+        private Dictionary<string, object> SerializeRowDr(IEnumerable<string> cols,
+                                                        SqlDataReader dr)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var col in cols)
+                result.Add(col, dr[col]);
+            return result;
+        }
+
+        private Dictionary<string, object> SerializeRowDr2(string Ids,
+                                                        SqlDataReader dr)
+        {
+
+
+            var result = new Dictionary<string, object>();
+            //foreach (var col in cols)
+              //  result.Add(col, dr[col]);
+            return result;
+        }
+
+    }
+}
--- OpenImis.RestApi/Controllers/V2/ServiceController.cs
+++ OpenImis.RestApi/Controllers/V2/ServiceController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OpenImis.Security.Security;
 using OpenImis.ModulesV2;
-using OpenImis.ModulesV2.ChequeModule;
+using OpenImis.ModulesV2.ServiceModule;
 using Newtonsoft.Json;
 
 namespace OpenImis.RestApi.Controllers.V2 { 
@@ -15,19 +15,19 @@ namespace OpenImis.RestApi.Controllers.V2 {
     [Route("api/")]
     [ApiController]
 
-    public class ChequeController : Controller
+    public class ServiceController : Controller
     {
-        public ChequeController()
+        public ServiceController()
         {
         }
         //[HasRights(Rights.DiagnosesDownload)]
         [HttpGet]   //To allow the sending of data that could be used by the consumers
-        [Route("GetListChequeItems")]
+        [Route("GetListServiceItems")]
 
 
-        public IActionResult GetListChequeItems()
+        public IActionResult GetListServiceItems()
         {
-            ClassChequeRequest getRequest = new ClassChequeRequest();
+            ClassServiceRequest getRequest = new ClassServiceRequest();
             var response = getRequest.SerializeDr();
             return Json(response);
         }
@@ -35,10 +35,12 @@ namespace OpenImis.RestApi.Controllers.V2 {
     }
 }
 
-/**************** Generated token to execute your Get Cheque until 07/07/2022 ********************************************************************************************
+/**************** Generated token to execute your Get Cheque until 08/08/2022 ********************************************************************************************
  * 
  * 
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyVVVJRCI6IjRhMWZiYjBlLWFhMzctNGRkYS04OTE0LTk3YzQ3YTViNzY2NSIsImV4cCI6MTY1Nzk2NTIxNiwiaXNzIjoiaHR0cDovL29wZW5pbWlzLm9yZyIsImF1ZCI6Imh0dHA6Ly9vcGVuaW1pcy5vcmcifQ.eSHHgI7O1UA3dhKioeZgvH8N3SDLc8tSdWnu0kfpGeE
+    "access_token": 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyVVVJRCI6IjRhMWZiYjBlLWFhMzctNGRkYS04OTE0LTk3YzQ3YTViNzY2NSIsImV4cCI6MTY2MDEyNTAxNiwiaXNzIjoiaHR0cDovL29wZW5pbWlzLm9yZyIsImF1ZCI6Imh0dHA6Ly9vcGVuaW1pcy5vcmcifQ.nmr29RUSbLcFYKACXp1c8kaLZBmoZJagMWwLj6kN__A",
+  "expires_on": "2022-08-10T10:50:16.8424129+01:00"
 
 *   API Version to provide in this case: 2
 
