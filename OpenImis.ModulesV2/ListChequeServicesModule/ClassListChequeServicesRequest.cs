b914ad369278ac6af9ef4e9641a7f8641b4c9a00
20GHhub19 <58400969+20GHhub19@users.noreply.github.com>
2022-08-24 14:46:56 +0100

    creating ClassListChequeServiceRequest File and updating SubItemController.cs file on develop-cs branch on 24_08_22

--- OpenImis.ModulesV2/ListChequeServicesModule/ClassListChequeServicesRequest.cs
+++ OpenImis.ModulesV2/ListChequeServicesModule/ClassListChequeServicesRequest.cs
@@ -13,6 +13,10 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using System.Xml;
 using System.Diagnostics;
+using OpenImis.ModulesV2.ServiceAllModule;
+using OpenImis.ModulesV2.MainItemModule;
+using OpenImis.ModulesV2.SubServiceModule;
+using OpenImis.ModulesV2.SubItemModule;
 
 namespace OpenImis.ModulesV2.ListChequeServicesModule
 {
@@ -21,10 +25,17 @@ namespace OpenImis.ModulesV2.ListChequeServicesModule
         public ClassListChequeServicesRequest()
         {
         }
-        public IEnumerable<List<Dictionary<string, object>>> ListChequeServ()
+        public List<IEnumerable<Dictionary<string, object>>> ListChequeServ()
         {
-            var results_list_all = new List<List<Dictionary<string, object>>>();
+            var results_list_all = new List<IEnumerable<Dictionary<string, object>>>();
 
+            ClassServiceAllRequest serviceAll = new ClassServiceAllRequest();
+            ClassMainItemRequest mainItem = new ClassMainItemRequest();
+            ClassSubServiceRequest subService = new ClassSubServiceRequest();
+            ClassSubItemRequest subItem = new ClassSubItemRequest();
+            var serv = serviceAll.SerializeDr(); var item = mainItem.SerializeDr(); var subserv = subService.SerializeDr(); var subIte = subItem.SerializeDr();
+
+            results_list_all.Add(serv); results_list_all.Add(item); results_list_all.Add(subserv); results_list_all.Add(subIte);
 
             return results_list_all;
         }
