0ce0c20aea8aba4b15cb2afd6d0093ff175ee17f
maxime ngoe <maxime.ngoe@gmail.com>
2023-01-26 14:12:33 +0100

    ajustement RegisterClaim + ClaimRepository

--- OpenImis.ModulesV3/ClaimModule/Models/RegisterClaim/Claim.cs
+++ OpenImis.ModulesV3/ClaimModule/Models/RegisterClaim/Claim.cs
@@ -54,5 +54,6 @@ namespace OpenImis.ModulesV3.ClaimModule.Models.RegisterClaim
         public string Code { get; set; }
         public decimal Quantity { get; set; }
         public decimal Price { get; set; }
+        public string Type { get; set; }
     }
 }
